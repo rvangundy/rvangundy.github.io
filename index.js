68c3e0ced1bd702f45dc1695c3a6fd90faf8e8d6
rvangundy <ryan.vangundy@gmail.com>
2013-12-06 19:14:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-
+  var div = document.createElement('div');
+  div.innerHTML = 'Trackback: <a href="' + document.referrer + '">' + document.referrer + '</a>';
+  
+  document.body.appendChild(div);
 });
